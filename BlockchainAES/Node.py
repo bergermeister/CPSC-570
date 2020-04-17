863d8db25771f50a043e1e82b500cbaa7a3a9363
Eisenberger, Edward <edward.eisenberger@live.com>
2020-04-17 13:29:44 -0400

    1. Added standard list of networked nodes 2. Implemented add_encrypted_transaction API 3. Implemented get_transactions API 4. Implemented get_decrypted_transactions API

--- BlockchainAES/Node.py
+++ BlockchainAES/Node.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from uuid import uuid4
 from urllib.parse import urlparse
+from Crypto.Cipher import AES
 from Block import Block
 from Chain import Chain
 import requests
@@ -15,6 +16,11 @@ host = ""
 port = 0
 network = [ ]                   # List of nodes in network
 networkFile = "./Network.json"  # Location to store list of nodes in network
+key = {
+   "17074dd6-09e7-4f6e-a8bd-b3744d397807" : bytes.fromhex( "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F" ),
+   "ceb627d5-7ae0-42e9-a9af-9fe76d0c0298" : bytes.fromhex( "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F" ),
+   "d95da554-75f9-489e-aecb-09673803bb66" : bytes.fromhex( "000102030405060708090A0B0C0D0E0F101112131415161718191A1B1C1D1E1F" )
+      }
 
 def Run( IPAddress, Port ):  
     global ID
@@ -30,6 +36,7 @@ def Run( IPAddress, Port ):
     ep = { 'uuid' : ID, 'hostname' : IPAddress, 'port' : Port }
     
     # Check if the network file exists
+    print( "Building Network List..." )
     if( os.path.isfile( networkFile ) and os.access( networkFile, os.R_OK ) ):
         # Read Network File
         with open( networkFile, 'r' ) as file:
@@ -40,38 +47,39 @@ def Run( IPAddress, Port ):
 
         # Search for current node in list of nodes
         found = False
-        print( "Read in Nodes:" )
         for node in data:
             print( node )
-            if( ( node[ 'hostname' ] == IPAddress ) and 
-                ( node[ 'port' ] == Port ) ):
+            if( ( node[ 'hostname' ] == IPAddress ) and ( node[ 'port' ] == Port ) ):
                 # If node is found, update the UUID
                 ID = node[ 'uuid' ]
                 ep[ 'uuid' ] = ID
                 found = True
-            else:
-                addr = 'http://' + node[ 'hostname' ] + ':' + str( node[ 'port' ] ) + '/connect_node'
-                try:
-                    requests.post( addr, json=ep )
-                except:
-                    print( f'Could not connect to {addr}' ) 
+                
             network.append( node )
                 
         # If node is not found, add new node to the list
         if( found == False ):
+            print( ep )
             network.append( ep )
         
     # If no network file exists, add this node as new node to the list
     else:
+        print( ep )
         network.append( ep )
     
     # Update the network file
     with open( networkFile, 'w' ) as file:
         json.dump( network, file, sort_keys = True, indent = 4 )
-    
-    print( "Nodes after startup" )
-    for key in network:
-        print( key )
+        
+    print( "Connecting to Nodes..." )
+    for node in network:
+        if( node[ 'uuid' ] != ID ):
+            addr = 'http://' + node[ 'hostname' ] + ':' + str( node[ 'port' ] ) + '/connect_node'
+            try:
+                requests.post( addr, json=ep )
+                print( f'Connected to {addr}' )
+            except:
+                print( f'Could not connect to {addr}' ) 
     
     # Run the Web Application
     web.run( host = IPAddress, port = Port )
@@ -168,11 +176,65 @@ def add_transaction():
 
 @web.route( '/add_encrypted_transaction', methods = ['POST'] )
 def add_encrypted_transaction():
-    return 
+    json = request.get_json()
+    transactionKeys = [ 'sender', 'receiver', 'data' ]
+    if not all(key in json for key in transactionKeys):
+        return 'Some elements of the transaction are missing', 400
+    sender = json[ 'sender' ]
+    receiver = json[ 'receiver' ]
+    data = json[ 'data' ]
+    if( ( sender == ID ) and ( receiver in key ) ):
+        dataLen = len( data )
+        pad = ( 16 - ( dataLen % 16 ) ) % 16
+        data = data.ljust( dataLen + pad, '~' )
+        cipher = AES.new( key[ receiver ] )
+        ciphertext = cipher.encrypt( data )
+        json[ 'data' ] = "AES-" + ciphertext.hex( )
+        index = chain.AddTransaction(json['sender'], json['receiver'], json['data'])
+        response = {'message': f'This transaction will be added to Block {index}',
+                    'transaction' : json }
+        code = 201
+    else:
+        response = { 'message' : 'Failed to add transaction to pool' }
+        code = 400
+    return( jsonify( response ), code ) 
 
-@web.route( '/decrypt_block', methods = ['POST'] )
-def decrypt_block():
-    return
+@web.route( '/get_transactions', methods = ['GET'] )
+def get_transactions( ):
+    data = [ ]
+    for block in chain.block:
+        for transaction in block.data:
+            if( ( transaction[ 'sender' ] == ID ) or ( transaction[ 'receiver' ] == ID ) ):
+                data.append( transaction )
+
+    response = { 'message' : f'Transactions for {ID}',
+                 'transactions' : data }
+    return( jsonify( response ), 200 )
+
+@web.route( '/get_decrypted_transactions', methods = ['GET'] )
+def get_decrypted_transactions( ):
+    data = [ ]
+    for block in chain.block:
+        for transaction in block.data:
+            sender = transaction[ 'sender' ]
+            receiver = transaction[ 'receiver' ]
+            if( transaction[ 'sender' ] == ID ):
+                data.append( transaction.copy( ) )   
+                if( transaction[ 'data' ].startswith( "AES-" ) ):
+                    cipher = AES.new( key[ receiver ] )
+                    plaintext = cipher.decrypt( bytes.fromhex( transaction[ 'data' ][ 4: ] ) )
+                    data[ - 1 ][ 'data' ] = plaintext.decode( )
+            elif( transaction[ 'receiver' ] == ID ):
+                data.append( transaction )
+                if( transaction[ 'data' ].startswith( "AES-" ) ):
+                    cipher = AES.new( key[ sender ] )
+                    plaintext = cipher.decrypt( bytes.fromhex( transaction[ 'data' ][ 4: ] ) )
+                    data[ - 1 ][ 'data' ] = plaintext.decode( )
+                    
+
+    response = { 'message' : f'Decrypted Transactions for {ID}',
+                 'transactions' : data }
+    return( jsonify( response ), 200 )
 
 # Connecting new nodes
 @web.route('/connect_node', methods = ['POST'])
