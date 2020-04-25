b7402813774a9a5279c749bdf18572cd5b9114de
Eisenberger, Edward <edward.eisenberger@live.com>
2020-04-25 12:50:50 -0400

    Updated compiler version and added storage for Diffie-Hellman Public Key Exchange and Encrypted data

--- SmartContract/EVLCoin.sol
+++ SmartContract/EVLCoin.sol
@@ -1,15 +1,28 @@
-pragma solidity >= 0.4.22 < 0.7.0;
+pragma solidity ^0.4.11;
 
 /**
  * EVLCoin Initial Coin Offering (ICO)
  */
 contract EVLCoin
 {
-    uint public constant Maximum = 1000000;     ///< Total number of EVLCoin tokens available
-    uint public RateUSD = 1000;                 ///< USD to EVLCoin Conversion RateUSD
-    uint public Purchased = 0;                  ///< Number of EVLCoins purchased by investors
+    /**
+     * Diffie-Hellman Public Key Exchange Structure
+     */ 
+    struct KeyExchange
+    {
+        uint256 p;      ///< Prime Number
+        uint256 g;      ///< Primitive Root Module p
+        uint256 A;      ///< Public Key A
+        uint256 B;      ///< Public Key B
+    }
+
+    uint public constant Maximum = 1000000;                             ///< Total number of EVLCoin tokens available
+    uint public RateUSD = 1000;                                         ///< USD to EVLCoin Conversion RateUSD
+    uint public Purchased = 0;                                          ///< Number of EVLCoins purchased by investors
     
-    mapping( address => uint ) private equity;  ///< Mapping from investor address to its equity in EVLCoin
+    mapping( address => uint ) private equity;                          ///< Mapping from investor address to its equity in EVLCoin
+    mapping( address => mapping( address => KeyExchange ) ) dhke;       ///< Diffie-Hellman Key Exchange
+    mapping( address => mapping( address => string ) ) encryptedData;   ///< Shared Encrypted Information
     
     /**
      * Modifier to check if an investor can buy EVLCoins
@@ -32,7 +45,7 @@ contract EVLCoin
     /**
      * Retrieve the equity in EVLCoins of an investor
      */ 
-    function GetEquity( address investor ) external view returns( uint )
+    function GetEquity( address investor ) external constant returns( uint )
     {
         return( equity[ investor ] );
     }
@@ -40,7 +53,7 @@ contract EVLCoin
     /**
      * Retrieve the equity in USD of an investor
      */ 
-    function GetEquityUSD( address investor ) external view returns( uint )
+    function GetEquityUSD( address investor ) external constant returns( uint )
     {
         return( equity[ investor ] / RateUSD );
     }
@@ -53,7 +66,7 @@ contract EVLCoin
     {
         uint coins = usd * RateUSD;
         equity[ investor ] += coins;
-        Purchased = coins;
+        Purchased += coins;
     }
 
     /**
@@ -64,6 +77,72 @@ contract EVLCoin
     {
         equity[ investor ] -= coins;
         Purchased -= coins;
+    }
+    
+    /**
+     * Initiates a Diffie-Hellman Public Key Exchange between requestor and responder
+     */
+    function RequestExchange( address requestor, address responder, uint256 p, uint256 g, uint256 A ) external
+    {
+        dhke[ requestor ][ responder ].p = p;
+        dhke[ requestor ][ responder ].g = g;
+        dhke[ requestor ][ responder ].A = A;
+    }
+    
+    /**
+     * Completes a Diffie-Hellman Public Key Exchange between requestor and responder
+     */
+    function RespondExchange( address requestor, address responder, uint256 B ) external
+    {
+        dhke[ requestor ][ responder ].B = B;
+    }
+    
+    /**
+     * Retrieves the Prime number used in the Diffie-Hellman Public Key Exchange between requestor and responder
+     */
+    function GetP( address requestor, address responder ) external constant returns( uint256 )
+    {
+        return( dhke[ requestor ][ responder ].p );
+    }
+    
+    /**
+     * Retrieves the Primitive Root Modulo p used in the Diffie-Hellman Public Key Exchange between requestor and responder
+     */
+    function GetG( address requestor, address responder ) external constant returns( uint256 )
+    {
+        return( dhke[ requestor ][ responder ].g );
+    }
+    
+    /**
+     * Retrieves the public key of the requestor in the Diffie-Hellman Public Key Exchange
+     */
+    function GetA( address requestor, address responder ) external constant returns( uint256 )
+    {
+        return( dhke[ requestor ][ responder ].A );
+    }
+    
+    /**
+     * Retrieves the public key of the responder in the Diffie-Hellman Public Key Exchange
+     */
+    function GetB( address requestor, address responder ) external constant returns( uint256 )
+    {
+        return( dhke[ requestor ][ responder ].B );
+    }
+    
+    /**
+     * Stores encrypted data sent from sender to receiver
+     */
+    function StoreData( address sender, address receiver, string data ) external 
+    {
+        encryptedData[ sender ][ receiver ] = data;
+    }
+    
+    /**
+     * Retrieves the encrypted data sent from sender to receiver
+     */
+    function GetData( address sender, address receiver ) external constant returns( string memory )
+    {
+        return( encryptedData[ sender ][ receiver ] );
     }
 }
 
